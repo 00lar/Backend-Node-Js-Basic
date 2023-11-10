38694fd1a942b5ae2cb96cdc13dea23427fb61d1
00lar <eduardani84@gmail.com>
2023-11-10 16:07:28 -0600

    CallbackHell

--- async/callbacks.js
+++ async/callbacks.js
@@ -1,22 +1,20 @@
 function hola(nombre, miCallback) {
-    setTimeout(function() {
-        console.log("Hola " + nombre);
-        miCallback(nombre);
-    },5000)
+  setTimeout(function () {
+    console.log('Hola ' + nombre)
+    miCallback(nombre)
+  }, 5000)
 }
 
-function adios (nombre, otroCallback) {
-    setTimeout( function() {
-        console.log("Adios " + nombre)
-        otroCallback()
-    }
-
-    )
+function adios(nombre, otroCallback) {
+  setTimeout(function () {
+    console.log('Adios ' + nombre)
+    otroCallback()
+  })
 }
 
-console.log("Inciando proceso...")
-    hola("daniel",function(nombre) {
-    adios(nombre, function() {
-        console.log("Proceso terminado")
-    })
+console.log('Inciando proceso...')
+hola('daniel', function (nombre) {
+  adios(nombre, function () {
+    console.log('Proceso terminado')
+  })
 })
--- concepts/monohilo.js
+++ concepts/monohilo.js
@@ -1,13 +1,13 @@
-console.log("Hola mundo!");
+console.log('Hola mundo!')
 
-let i = 0;
-setInterval(function() {
-    console.log(i);
-    i++;
-    if (i === 5) {
-        console.log("forzamos error");
-        var a = 3 + z;
-    }
+let i = 0
+setInterval(function () {
+  console.log(i)
+  i++
+  if (i === 5) {
+    console.log('forzamos error')
+    var a = 3 + z
+  }
 }, 1000)
 
-console.log("Segunda instrucción");
+console.log('Segunda instrucción')
